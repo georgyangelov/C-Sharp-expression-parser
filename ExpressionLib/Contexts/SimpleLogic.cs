fce40dd4c922ee8b1066af1ad3a96fa770741fbd
Georgy Angelov <georgyangelov@gmail.com>
2011-10-12 00:28:51 +0300

    Simple logic context class. Defines boolean operators:
    - ! - not
    - & - and
    - | - or
    - > - implication
    - = - bi-implication

---
+++ ExpressionLib/Contexts/SimpleLogic.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ExpressionLib.Contexts
+{
+    public class SimpleLogic : IContext<bool>
+    {
+        public int NumParams(string oprtr)
+        {
+            if (oprtr == "!")
+                return 1;
+            else
+                return 2;
+        }
+
+        public bool EvalOperator(string oprtr, List<bool> values)
+        {
+            if (oprtr == "!")
+                return !values[0];
+            else if (oprtr == "&")
+                return (values[0] && values[1]);
+            else if (oprtr == "|")
+                return (values[0] || values[1]);
+            else if (oprtr == ">")
+                return (!values[0] || values[1]);
+            else if (oprtr == "=")
+                return (values[0] == values[1]);
+            else
+                throw new ParsingException("Unknown operator '" + oprtr + "' detected!");
+        }
+
+        public bool IsValue(string c)
+        {
+            bool r;
+            return IsValue(c, out r);
+        }
+
+        public bool IsValue(string c, out bool r)
+        {
+            if (c == "T" || c == "1")
+            {
+                r = true;
+
+                return true;
+            }
+            else if (c == "F" || c == "0")
+            {
+                r = false;
+
+                return true;
+            }
+            else
+            {
+                r = false;
+                return false;
+            }
+        }
+
+        public bool IsValidIdentificator(string c)
+        {
+            return Regex.IsMatch(c, @"^[a-zA-Z_]([a-zA-Z0-9_]+)?$");
+        }
+
+        public bool IsOperator(string c)
+        {
+            return
+            (
+                c == "!" || c == "&" || c == "|" || c == ">" || c == "="
+            );
+        }
+
+        public int PriorityOf(string c)
+        {
+            if (c == "!")
+                return 5;
+            else if (c == "&")
+                return 4;
+            else if (c == "|")
+                return 3;
+            else if (c == ">")
+                return 2;
+            else if (c == "=")
+                return 1;
+            else return 0;
+        }
+
+        public Associativity AssociativityOf(string c)
+        {
+            return Associativity.Left;
+        }
+    }
+}
--- LogicalOperators/Program.cs
+++ LogicalOperators/Program.cs
@@ -12,13 +12,32 @@ namespace LogicalOperators
         {
             Evaluator<double> eval = new Evaluator<double>(new ExpressionLib.Contexts.SimpleMath());
 
-            string expr = "-1/2+(-3*2)^2";
+            string expr = "-1/2+(-3*2)^2 * sqrt(2+2)";
 
             try
             {
-                Console.WriteLine(expr);
-                Console.WriteLine(eval.ToPostfix(expr));
-                Console.WriteLine(eval.EvalInfix(expr));
+                Console.WriteLine("Sample math expression: " + expr);
+                Console.WriteLine("In postfix format: " + eval.ToPostfix(expr));
+                Console.WriteLine("Result: " + eval.EvalInfix(expr));
+            }
+            catch (ParsingException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            Console.WriteLine("----------------------------------------");
+
+            Evaluator<bool> evalLogic = new Evaluator<bool>(new ExpressionLib.Contexts.SimpleLogic());
+
+            string exprLogic = "(F | (!T > T)) > ((F & T) > (F > T))";
+
+            try
+            {
+                Console.WriteLine("Sample logical expression: " + exprLogic);
+                Console.WriteLine("In postfix format: " + evalLogic.ToPostfix(exprLogic));
+                Console.WriteLine("Result: " + evalLogic.EvalInfix(exprLogic));
             }
             catch (ParsingException e)
             {
